c49bd733051555c16aadda76d58153e7f4ba6cbe
wirs <wiirrss@gmail.com>
2013-03-19 17:36:55 +0100

    initial

---
+++ project-minigolf/src/minigolf/LoadDriver.java
@@ -0,0 +1,35 @@
+package minigolf;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Notice, do not import com.mysql.jdbc.*
+// or you will have problems!
+public class LoadDriver {
+    public static void main(String[] args) {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            System.out.println("Driver load failed");
+        }
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://admin.4pals.org/minigolf_db?" + "user=golf_user&password=g0lfp4$$");
+            // Do something with the Connection
+           System.out.println("Dass alles ok");
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    
+    }
+}
+
+
+
